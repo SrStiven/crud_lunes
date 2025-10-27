b021a0abbb9316c51ce59f31f1acee3129e8331c
David Estiven Mendez Lara <estivenmendezlara2020@gmail.com>
2025-10-27 17:25:05 -0500

    Semilla de 20 libros aleatorios

---
+++ database/seeders/BooksSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Books;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class BooksSeeder extends Seeder
+{
+    
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        for($i=0;$i <20; $i++){
+            Books::create([
+                'name' => $faker -> name(),
+                'title' => $faker -> sentence(3),
+                'age' => $faker -> numberBetween(0,3000),
+                'count' => $faker -> numberBetween(0,100),
+                'gender' => $faker -> randomElement(['accion', 'ficcion', 'comedia'])
+            ]);
+        }
+
+    }
+}
